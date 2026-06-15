e176901def56261c1e0ba9a865e0fdf0b952efc6
alex <alexandru.radu@enterteq.eu>
2026-06-15 17:53:55 +0300

    fix config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  // base: "/lissa-web/", // 👈 THIS is the important line
+  base: "/lissa-web/", // 👈 THIS is the important line
   plugins: [react()],
 });
